427fa439f4a9f8c426d2ec230c7bb03a08772df7
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-08-06 22:29:28 +0200

    vec tests, deref_mut, no unsafe code

--- src/vec.rs
+++ src/vec.rs
@@ -4,7 +4,7 @@
 
 use core::marker::PhantomData;
 use core::ops::Deref;
-use core::slice;
+use core::ops::DerefMut;
 
 /// A continuous, growable array type
 pub struct Vec<T, A>
@@ -41,6 +41,11 @@ where
         self.len = 0;
     }
 
+    /// Returns the current length of this vector
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
     /// Removes the last element from this vector and returns it, or `None` if
     /// it's empty
     pub fn pop(&mut self) -> Option<T> {
@@ -48,7 +53,7 @@ where
             None
         } else {
             self.len -= 1;
-            unsafe { Some(*self.array.as_mut().as_mut_ptr().offset(self.len as isize)) }
+            Some(self.array.as_ref()[self.len])
         }
     }
 
@@ -61,9 +66,7 @@ where
         if self.len == slice.len() {
             Err(())
         } else {
-            unsafe {
-                *slice.as_mut_ptr().offset(self.len as isize) = elem;
-            }
+            slice[self.len] = elem;
             self.len += 1;
             Ok(())
         }
@@ -78,6 +81,107 @@ where
     type Target = [T];
 
     fn deref(&self) -> &[T] {
-        unsafe { slice::from_raw_parts(self.array.as_ref().as_ptr(), self.len) }
+        self.array.as_ref()[..(self.len)].as_ref()
     }
+}
+
+impl<T, A> DerefMut for Vec<T, A>
+where
+    A: AsMut<[T]> + AsRef<[T]>,
+    T: Copy,
+{
+
+    fn deref_mut(&mut self) -> &mut [T] {
+        self.array.as_mut()[..(self.len)].as_mut()
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use core::ops::Deref;
+    use core::ops::DerefMut;
+
+    use super::Vec;
+    
+    #[test]
+    fn compile_time_const_new() {
+        const LEN: usize = 16; 
+        const VEC: Vec<u8, [u8; LEN]> = Vec::new([0;LEN]);
+        assert_eq!(VEC.capacity(), LEN);
+    }
+
+    #[test]
+    fn compile_time_static_new() {
+        const LEN: usize = 16; 
+        static VEC: Vec<u8, [u8; LEN]> = Vec::new([0;LEN]);
+        assert_eq!(VEC.capacity(), LEN);
+    }
+
+    #[test]
+    fn initial_length() {
+        const LEN: usize = 16; 
+        let vec = Vec::new([0;LEN]);
+        assert_eq!(vec.len(), 0);
+    }
+
+    #[test]
+    fn push_length() {
+        const LEN: usize = 16; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert_eq!(vec.len(), 1);
+    }
+
+    #[test]
+    fn push_pop() {
+        const LEN: usize = 16; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert_eq!(vec.pop(), Some(1));
+        assert_eq!(vec.len(), 0);
+    }
+
+    #[test]
+    fn empty_pop() {
+        const LEN: usize = 16; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert_eq!(vec.pop(), Some(1));
+        assert_eq!(vec.len(), 0);
+        assert_eq!(vec.pop(), None);
+        assert_eq!(vec.len(), 0);
+    }
+
+    #[test]
+    fn full_push() {
+        const LEN: usize = 2; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert!(vec.push(2).is_ok());
+        assert_eq!(vec.len(), 2);
+        assert!(vec.push(3).is_err());
+        assert_eq!(vec.len(), 2);
+    }
+
+    #[test]
+    fn deref() {
+        const LEN: usize = 3; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert!(vec.push(2).is_ok());
+        assert_eq!(vec.deref(), &[1, 2]);
+    }
+
+    #[test]
+    fn deref_mut() {
+        const LEN: usize = 3; 
+        let mut vec = Vec::new([0;LEN]);
+        assert!(vec.push(1).is_ok());
+        assert!(vec.push(2).is_ok());
+        vec.deref_mut()[0] = 3;
+        vec.deref_mut()[1] = 2;
+        assert_eq!(vec.deref(), &[3,2]);
+    }
+
 }
