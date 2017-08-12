71cb59a3948ed9766e5027a502710b455fe136be
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-08-12 15:28:48 +0200

    circular buffer docs

--- src/circular_buffer.rs
+++ src/circular_buffer.rs
@@ -24,6 +24,24 @@ where
     T: Copy,
 {
     /// Creates a new empty circular buffer using `array` as backup storage.
+    ///
+    /// # Example
+    /// ```
+    /// #![feature(const_fn)]
+    /// use heapless::CircularBuffer;
+    /// const LEN: usize = 16;
+    /// // with let
+    /// let buf_let = CircularBuffer::new([0;LEN]);
+    /// // with const
+    /// const BUF_CONST: CircularBuffer<u8, [u8; LEN]>
+    ///     = CircularBuffer::new([0;LEN]);
+    /// // with static
+    /// static BUF_STATIC: CircularBuffer<u8, [u8; LEN]>
+    ///     = CircularBuffer::new([0;LEN]);
+    /// // with slice
+    /// let mut array = [0_usize; 8];
+    /// let mut buf_slice = CircularBuffer::new(&mut array);
+    /// ```
     pub const fn new(array: A) -> Self {
         CircularBuffer {
             _marker: PhantomData,
@@ -37,6 +55,13 @@ where
     /// Keeps the values and the length of the array.
     /// The backup storage is completly used upon creation.
     /// Insertions will overwrite at the start of the array.
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// let buf = CircularBuffer::from_array([1,2,3]);
+    /// assert!(buf.is_full());
+    /// ```
     pub fn from_array(array: A) -> Self {
         let len = array.as_ref().len();
         CircularBuffer {
@@ -48,22 +73,52 @@ where
     }
 
     /// Returns the capacity of this buffer.
+    ///
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// const LEN: usize = 16;
+    /// let buf = CircularBuffer::new([0;LEN]);
+    /// assert_eq!(buf.capacity(), LEN);
+    /// ```
     pub fn capacity(&self) -> usize {
         self.array.as_ref().len()
     }
 
     /// Returns the current length of this buffer.
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// let buf = CircularBuffer::from_array([1,2,3]);
+    /// assert_eq!(buf.len(), 3);
+    /// ```
     pub fn len(&self) -> usize {
         self.len
     }
 
     /// Returns the current index of this buffer.
     /// This is the next place to push to.
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// let buf = CircularBuffer::from_array([1,2,3]);
+    /// assert_eq!(buf.index(), 0);
+    /// ```
     pub fn index(&self) -> usize {
         self.index
     }
 
     /// Returns `true` if the buffer is empty.
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// let buf = CircularBuffer::new([0;8]);
+    /// assert!(buf.is_empty());
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
@@ -72,6 +127,13 @@ where
     ///
     /// Note that elements can still be inserted but will overwrite
     /// the last ones.
+    ///
+    /// # Example
+    /// ```
+    /// use heapless::CircularBuffer;
+    /// let buf = CircularBuffer::from_array([1,2,3]);
+    /// assert!(buf.is_full());
+    /// ```
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
     }
@@ -96,7 +158,7 @@ where
     /// Sets the length of the buffer.
     ///
     /// This is unsafe because no checks are performed, memory can be leaked
-    /// or uninitialized memory mey be exposed.
+    /// or uninitialized memory may be exposed.
     unsafe fn set_len(&mut self, len: usize) {
         self.len = len;
     }
@@ -105,7 +167,7 @@ where
     /// This is the next place to insert to.
     ///
     /// This is unsafe because memory can be leaked
-    /// or uninitialized memory mey be exposed.
+    /// or uninitialized memory may be exposed.
     unsafe fn set_index(&mut self, index: usize) {
         self.index = index % self.capacity();
     }
