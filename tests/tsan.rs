9533e27f44b21eb331188074771610eb9dae661b
Jorge Aparicio <jorge@japaric.io>
2017-11-08 22:51:55 +0100

    test two consecutive operations

--- tests/tsan.rs
+++ tests/tsan.rs
@@ -5,7 +5,7 @@ use std::thread;
 use heapless::RingBuffer;
 
 #[test]
-fn tsan() {
+fn once() {
     static mut RB: RingBuffer<i32, [i32; 4]> = RingBuffer::new();
 
     unsafe { RB.split() }.0.enqueue(0).unwrap();
@@ -15,6 +15,24 @@ fn tsan() {
     });
 
     thread::spawn(|| {
+        unsafe { RB.split() }.1.dequeue().unwrap();
+    });
+}
+
+#[test]
+fn twice() {
+    static mut RB: RingBuffer<i32, [i32; 8]> = RingBuffer::new();
+
+    unsafe { RB.split() }.0.enqueue(0).unwrap();
+    unsafe { RB.split() }.0.enqueue(1).unwrap();
+
+    thread::spawn(|| {
+        unsafe { RB.split() }.0.enqueue(2).unwrap();
+        unsafe { RB.split() }.0.enqueue(3).unwrap();
+    });
+
+    thread::spawn(|| {
+        unsafe { RB.split() }.1.dequeue().unwrap();
         unsafe { RB.split() }.1.dequeue().unwrap();
     });
 }
