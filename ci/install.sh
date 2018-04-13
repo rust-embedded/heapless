301c1becdfe7da185b3e5a2a67ce2ae6e60047ba
Jorge Aparicio <jorge@japaric.io>
2018-04-13 16:26:16 +0200

    drop Xargo

--- ci/install.sh
+++ ci/install.sh
@@ -1,21 +1,9 @@
 set -euxo pipefail
 
 main() {
-    # install latest Xargo
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/xargo \
-                    | cut -d/ -f3 \
-                    | grep -E '^v[0-9.]+$' \
-                    | sort --version-sort \
-                    | tail -n1)
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/xargo \
-           --target x86_64-unknown-linux-musl \
-           --tag $tag
-
-    rustup component list | grep 'rust-src.*installed' || \
-        rustup component add rust-src
+    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+        rustup target add $TARGET
+    fi
 }
 
 main
