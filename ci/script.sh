6c28c8f62a6a0c91266e8eb9b1ac2b9aca252ea7
Felix Stegmaier <felix.stegmaier@hpe.com>
2018-07-13 11:28:58 +0200

    make feature const-fn default

--- ci/script.sh
+++ ci/script.sh
@@ -5,18 +5,18 @@ main() {
 
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
         cargo test --target $TARGET
-        cargo test --target $TARGET --all-features
+        cargo test --target $TARGET --no-default-features
         cargo test --target $TARGET --release
-        cargo test --target $TARGET --release --all-features
+        cargo test --target $TARGET --release --no-default-features
 
         export RUSTFLAGS="-Z sanitizer=thread"
         export RUST_TEST_THREADS=1
         export TSAN_OPTIONS="suppressions=$(pwd)/blacklist.txt"
 
         cargo test --test tsan --target $TARGET
-        cargo test --test tsan --target $TARGET --all-features
+        cargo test --test tsan --target $TARGET --no-default-features
         cargo test --test tsan --target $TARGET --release
-        cargo test --test tsan --target $TARGET --release --all-features
+        cargo test --test tsan --target $TARGET --release --no-default-features
     fi
 }
 
