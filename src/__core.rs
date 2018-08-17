343a8c6a6361de122a0981d2f98fa64c2b7f4d51
Felix Stegmaier <felix.stegmaier@hpe.com>
2018-08-17 15:46:52 +0200

    no-default-features friendly uninit

--- src/__core.rs
+++ src/__core.rs
@@ -1,18 +1,20 @@
 /// Temporary fork of some stuff in `core` that's doesn't have a `const fn` API
 
 pub mod mem {
-    pub use core::mem::{replace, zeroed, ManuallyDrop};
+    pub use core::mem::{replace, zeroed, ManuallyDrop, uninitialized};
     use core::ops::{Deref, DerefMut};
 
 
     /// extremely unsafe uniniatilized memory
     /// only use with ManuallyDrop
     #[allow(unions_with_drop_fields)]
+    #[cfg(feature = "const-fn")]
     pub(crate) union Uninit<T> {
         uninit: (),
         init: T,
     }
 
+    #[cfg(feature = "const-fn")]
     impl<T> Uninit<T> {
         const_fn!(
             pub const unsafe fn new() -> Self {
@@ -23,6 +25,7 @@ pub mod mem {
         );
     }
 
+    #[cfg(feature = "const-fn")]
     impl<T> Deref for Uninit<T> {
         type Target = T;
         fn deref(&self) -> &T {
@@ -30,11 +33,40 @@ pub mod mem {
         }
     }
 
+    #[cfg(feature = "const-fn")]
     impl<T> DerefMut for Uninit<T> {
         fn deref_mut(&mut self) -> &mut T {
             unsafe { &mut self.init }
         }
     }
+
+    /// extremely unsafe uniniatilized memory
+    /// only use with ManuallyDrop
+    #[cfg(not(feature = "const-fn"))]
+    pub(crate) struct Uninit<T>(T);
+
+    #[cfg(not(feature = "const-fn"))]
+    impl<T> Uninit<T> {
+        pub unsafe fn new() -> Self {
+            Uninit(uninitialized())
+        }
+    }
+
+    #[cfg(not(feature = "const-fn"))]
+    impl<T> Deref for Uninit<T> {
+        type Target = T;
+        fn deref(&self) -> &T {
+            &self.0
+        }
+    }
+
+    #[cfg(not(feature = "const-fn"))]
+    impl<T> DerefMut for Uninit<T> {
+        fn deref_mut(&mut self) -> &mut T {
+            &mut self.0
+        }
+    }
+
 
 }
 
