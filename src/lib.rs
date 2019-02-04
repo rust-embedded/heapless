82fded5155df73cb51e1d50210000c966b503609
Jorge Aparicio <jorge@japaric.io>
2019-02-04 19:23:47 +0100

    remove no longer needded integer_atomics feature

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,6 @@
 #![deny(warnings)]
 #![cfg_attr(feature = "const-fn", feature(const_fn))]
 #![cfg_attr(feature = "const-fn", feature(untagged_unions))]
-#![cfg_attr(feature = "smaller-atomics", feature(integer_atomics))]
 #![no_std]
 
 extern crate generic_array;
