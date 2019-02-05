3cf3b77e4378742b5dfc5a7d50d550718b089873
Jorge Aparicio <jorge@japaric.io>
2019-02-05 10:34:34 +0100

    tweak ci/script.sh to make it easier to run it locally

--- ci/script.sh
+++ ci/script.sh
@@ -26,6 +26,33 @@ main() {
     fi
 }
 
+# fake Travis variables to be able to run this on a local machine
+if [ -z ${TRAVIS_BRANCH-} ]; then
+    TRAVIS_BRANCH=auto
+fi
+
+if [ -z ${TRAVIS_PULL_REQUEST-} ]; then
+    TRAVIS_PULL_REQUEST=false
+fi
+
+if [ -z ${TRAVIS_RUST_VERSION-} ]; then
+    case $(rustc -V) in
+        *nightly*)
+            TRAVIS_RUST_VERSION=nightly
+            ;;
+        *beta*)
+            TRAVIS_RUST_VERSION=beta
+            ;;
+        *)
+            TRAVIS_RUST_VERSION=stable
+            ;;
+    esac
+fi
+
+if [ -z ${TARGET-} ]; then
+    TARGET=$(rustc -Vv | grep host | cut -d ' ' -f2)
+fi
+
 if [ $TRAVIS_BRANCH != master ]; then
     main
 fi
