ea0847ad42a8db5d71749187afe645f3b5f088d7
Artem Vorotnikov <artem@vorotnikov.me>
2019-02-06 18:43:19 +0300

    Test serde feature on stable

--- ci/script.sh
+++ ci/script.sh
@@ -1,27 +1,27 @@
 set -euxo pipefail
 
 main() {
-    cargo check --target $TARGET
+    cargo check --target $TARGET --features 'serde'
     if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo check --target $TARGET --features 'serde const-fn smaller-atomics'
+        cargo check --target $TARGET --features 'const-fn smaller-atomics'
     fi
 
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        cargo test --target $TARGET
-        cargo test --target $TARGET --release
+        cargo test --target $TARGET --features 'serde'
+        cargo test --target $TARGET --release --features 'serde'
 
         if [ $TRAVIS_RUST_VERSION = nightly ]; then
-            cargo test --target $TARGET --features 'serde const-fn smaller-atomics'
-            cargo test --target $TARGET --release --features 'serde const-fn smaller-atomics'
+            cargo test --target $TARGET --features 'const-fn smaller-atomics'
+            cargo test --target $TARGET --release --features 'const-fn smaller-atomics'
 
             export RUSTFLAGS="-Z sanitizer=thread"
             export RUST_TEST_THREADS=1
             export TSAN_OPTIONS="suppressions=$(pwd)/blacklist.txt"
 
             cargo test --test tsan --target $TARGET
-            cargo test --test tsan --target $TARGET --features 'serde const-fn smaller-atomics'
+            cargo test --test tsan --target $TARGET --features 'const-fn smaller-atomics'
             cargo test --test tsan --target $TARGET --release
-            cargo test --test tsan --target $TARGET --release --features 'serde const-fn smaller-atomics'
+            cargo test --test tsan --target $TARGET --release --features 'const-fn smaller-atomics'
         fi
     fi
 }
