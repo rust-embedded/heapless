e42463941996dacc2d4f27ecaacd481157ae4eed
Felix Stegmaier <stegmaier.felix@gmail.com>
2019-02-17 12:42:43 +0100

    Improve docs of CapacityResult

--- src/errors.rs
+++ src/errors.rs
@@ -31,10 +31,22 @@ impl CapacityError {
 
 }
 
-/// Result returned from insertion operation
-/// Generic over the rest of the data, that cound not be inserted
+/// Result returned from insertion operation.
+/// The insertion operation can either succeed
+/// or fail due to an capacity overflow.
+///
+/// If the operation suceeded, the result is ok,
+/// containing an value of thype `T`.
+/// For many insert operation this value is `()`,
+/// however if the insertion replaced an existing value in an collection,
+/// the return value might be `Some(replaced_value)`.
+///
+/// If the operation failed fue to an capacity overflow,
+/// this result contains two pieces of data
+///   - an `CapacityError`, which describes by how for the capacity was exceeded
+///   - an `rest` of type `R`, which is the rest of the data, that could not be inserted.
 #[derive(Debug)]
-#[must_use = "this `Capacity result might be an error vairant and must be used"]
+#[must_use = "this `CapacityResult` might be an error variant and must be used"]
 pub struct CapacityResult<T, R> (Result<T, (R, CapacityError)>);
 
 impl<T, R> CapacityResult<T, R> {
@@ -66,11 +78,11 @@ impl<T, R> CapacityResult<T, R> {
     ///
     /// This method can be used to perform error handling,
     /// when the returned `rest` is not relevant.
-    /// The returned result can by used with the `?` operator
+    /// The returned result can by used with the `?` operator.
     ///
     /// # Examples
     ///
-    /// basic usage
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -83,7 +95,7 @@ impl<T, R> CapacityResult<T, R> {
     /// assert_eq!(b.into_result(), Err(CapacityError::one_more_than(1)));
     /// ```
     ///
-    /// For error handling
+    /// For error handling:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -102,7 +114,7 @@ impl<T, R> CapacityResult<T, R> {
     }
 
     /// Use the result as an optional reference to `rest`,
-    /// which could not be inserted due to capacity errors
+    /// which could not be inserted due to capacity errors.
     pub fn as_rest(&self) -> Option<&R> {
         self.0.as_ref()
             .err()
@@ -110,7 +122,7 @@ impl<T, R> CapacityResult<T, R> {
     }
 
     /// Use the result as an optional mutable reference to `rest`,
-    /// which could not be inserted due to capacity errors
+    /// which could not be inserted due to capacity errors.
     pub fn as_mut_rest(&mut self) -> Option<&mut R> {
         self.0.as_mut()
             .err()
@@ -118,14 +130,14 @@ impl<T, R> CapacityResult<T, R> {
     }
 
     /// Convert the result to an optional `rest`,
-    /// which could not be inserted due to capacity errors
+    /// which could not be inserted due to capacity errors.
     ///
     /// This method can be used to try to recover from the given CapacityError
-    /// by performing some operation with the element that could not be inserted
+    /// by performing some operation with the element that could not be inserted.
     ///
     /// # Examples
     ///
-    /// Basic usage
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -172,9 +184,11 @@ impl<T, R> CapacityResult<T, R> {
             .map(|e| &mut e.1)
     }
 
-    /// Convert the result to an optional error
+    /// Convert the result to an optional error.
     ///
     /// # Examples
+    ///
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -187,9 +201,11 @@ impl<T, R> CapacityResult<T, R> {
             .map(|e| e.1)
     }
 
-    /// returns `true` if the result is ok
+    /// Returns `true` if the result is ok.
     ///
     /// # Examples
+    ///
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -201,9 +217,11 @@ impl<T, R> CapacityResult<T, R> {
         self.0.is_ok()
     }
 
-    /// returns `true` if the result is err
+    /// Returns `true` if the result is an error.
     ///
     /// # Examples
+    ///
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -216,13 +234,15 @@ impl<T, R> CapacityResult<T, R> {
         self.0.is_err()
     }
 
-    /// Unwrap the result, panic if was an error
+    /// Unwrap the result to access the contained value.
     ///
     /// # Panics
     ///
-    /// Panics if it was an error
+    /// Panics if the result was an error.
     ///
     /// # Examples
+    ///
+    /// Basic usage:
     ///
     /// ```
     /// use heapless::CapacityResult;
@@ -230,6 +250,8 @@ impl<T, R> CapacityResult<T, R> {
     /// let x: CapacityResult<(), i32> = CapacityResult::ok(());
     /// x.unwrap(); // does not panic
     /// ```
+    ///
+    /// Panic on error:
     ///
     /// ```should_panic
     /// use heapless::CapacityResult;
@@ -242,29 +264,74 @@ impl<T, R> CapacityResult<T, R> {
         self.into_result().unwrap()
     }
 
-    /// Unwrap the result and panic with the given message if it was an error
+    /// Unwrap the result to access the contained value
+    /// and panic with the given message if it was an error.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the result was an error
     pub fn expect(self, msg: &str) -> T {
         self.into_result().expect(msg)
     }
 
-    /// Ignore this result, but use it.
-    /// this serves the same purpose as calling `Result::ok()`
-    /// but not using the value
+    /// Ignore this result, but consume it.
+    ///
+    /// This serves the same purpose as calling `Result::ok()`
+    /// but not using the `ok` value.
     pub fn ignore(self) {
         self.0.ok();
     }
 
-    /// If the result is an error, transform the contained rest
+    /// If the result is an error, transform the contained rest.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use heapless::CapacityResult;
+    /// use heapless::CapacityError;
+    ///
+    /// let x: CapacityResult<(), i32> = CapacityResult::err(42, CapacityError::one_more_than(1));
+    /// assert_eq!(x.map_rest(|i| i+1).into_rest(), Some(43));
+    /// ```
     pub fn map_rest<S, F: FnOnce(R) -> S>(self, f: F) -> CapacityResult<T, S> {
         CapacityResult(self.0.map_err(|(rest, err)| (f(rest), err)))
     }
 
     /// If the result is ok, transform the value
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use heapless::CapacityResult;
+    /// use heapless::CapacityError;
+    ///
+    /// let x: CapacityResult<i32, i32> = CapacityResult::ok(42);
+    /// assert_eq!(x.map(|i| i+1).into_result().ok(), Some(43));
+    /// ```
     pub fn map<U, F: FnOnce(T) -> U>(self, f: F) -> CapacityResult<U, R> {
         CapacityResult(self.0.map(f))
     }
 
-    /// Transform the error variant, ignore the contained rest
+    /// Transform the error variant, ignore the contained rest.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use heapless::CapacityResult;
+    /// use heapless::CapacityError;
+    ///
+    /// #[derive(Debug, PartialEq, Eq)]
+    /// struct FooError;
+    ///
+    /// let x: CapacityResult<(), i32> = CapacityResult::err(42, CapacityError::one_more_than(1));
+    /// assert_eq!(x.map_err(|_| FooError).err(), Some(FooError));
+    /// ```
     pub fn map_err<E, F: FnOnce(CapacityError) -> E>(self, f: F) -> Result<T, E> {
         self.0.map_err(|(_, err)| f(err))
     }
