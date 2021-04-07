2ade7797cb51f9c1bb1b433c8b8a23f3e45d7243
YuhanLiin <yuhanliin+github@protonmail.com>
2021-04-06 20:01:30 -0400

    Add valid Vec types to cpass

--- tests/cpass.rs
+++ tests/cpass.rs
@@ -1,13 +1,15 @@
-//! Collections of `Send`-able things are `Send`
+use std::marker::PhantomData;
 
+use generic_array::typenum::op;
 use heapless::{
-    consts,
+    consts::*,
     spsc::{Consumer, Producer, Queue},
     HistoryBuffer, Vec,
 };
 
 #[test]
 fn send() {
+    // Collections of `Send`-able things are `Send`
     struct IsSend;
 
     unsafe impl Send for IsSend {}
@@ -18,9 +20,20 @@ fn send() {
     {
     }
 
-    is_send::<Consumer<IsSend, consts::U4>>();
-    is_send::<Producer<IsSend, consts::U4>>();
-    is_send::<Queue<IsSend, consts::U4>>();
-    is_send::<Vec<IsSend, consts::U4>>();
-    is_send::<HistoryBuffer<IsSend, consts::U4>>();
+    is_send::<Consumer<IsSend, U4>>();
+    is_send::<Producer<IsSend, U4>>();
+    is_send::<Queue<IsSend, U4>>();
+    is_send::<Vec<IsSend, U4, u8>>();
+    is_send::<HistoryBuffer<IsSend, U4>>();
+}
+
+#[test]
+fn vec() {
+    // Vecs that are sized within the bounds of their length types
+    let _s: PhantomData<Vec<u8, U200, u8>>;
+    let _s: PhantomData<Vec<u8, U255, u8>>;
+    let _s: PhantomData<Vec<u8, U256, u16>>;
+    let _s: PhantomData<Vec<u8, op!(U65536 - U1), u16>>;
+    let _s: PhantomData<Vec<u8, U65536, usize>>;
+    let _s: PhantomData<Vec<u8, U100000, usize>>;
 }
