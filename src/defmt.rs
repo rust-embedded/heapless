63d0ed83953c62addd2d9adbeefc09b4be51f1ac
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-05-18 11:11:30 -0700

    cargo fmt

--- src/defmt.rs
+++ src/defmt.rs
@@ -5,8 +5,8 @@ use crate::Vec;
 use defmt::Formatter;
 
 impl<T, const N: usize> defmt::Format for Vec<T, N>
-    where
-        T: defmt::Format,
+where
+    T: defmt::Format,
 {
     fn format(&self, fmt: Formatter<'_>) {
         defmt::write!(fmt, "{=[?]}", self.as_slice())
@@ -14,8 +14,8 @@ impl<T, const N: usize> defmt::Format for Vec<T, N>
 }
 
 impl<const N: usize> defmt::Format for crate::String<N>
-    where
-        u8: defmt::Format,
+where
+    u8: defmt::Format,
 {
     fn format(&self, fmt: Formatter<'_>) {
         defmt::write!(fmt, "{=str}", self.as_str());
