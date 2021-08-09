28a32b7ad92d05144ab01ba9f6ce81f6be4272e8
korken89 <korken89@users.noreply.github.com>
2021-08-09 08:28:19 +0000

    deploy: 8d76c2d755a5b0877a60546c9507c56846fd80ce

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["byteorder"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["hash32"] = {"name":"","files":["fnv.rs","lib.rs","murmur3.rs"]};
-sourcesIndex["heapless"] = {"name":"","dirs":[{"name":"pool","files":["cas.rs","mod.rs","singleton.rs"]}],"files":["binary_heap.rs","histbuf.rs","indexmap.rs","indexset.rs","lib.rs","linear_map.rs","mpmc.rs","sealed.rs","spsc.rs","string.rs","vec.rs"]};
+sourcesIndex["heapless"] = {"name":"","dirs":[{"name":"pool","files":["cas.rs","mod.rs","singleton.rs"]}],"files":["binary_heap.rs","deque.rs","histbuf.rs","indexmap.rs","indexset.rs","lib.rs","linear_map.rs","mpmc.rs","sealed.rs","spsc.rs","string.rs","vec.rs"]};
 sourcesIndex["stable_deref_trait"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
