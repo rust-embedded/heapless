eca85d287fb99dc0c89648c35ee65103be90544c
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2021-10-01 13:35:39 -0700

    Bugfix.  Needed to store the write_at index to a temporary variable.

--- src/histbuf.rs
+++ src/histbuf.rs
@@ -184,7 +184,8 @@ impl<T, const N: usize> HistoryBuffer<T, N> {
 
     // Re-arranges the data so that the underlying array slice is ordered oldest to newest
     pub fn order(&mut self) {
-        for i in 0..self.write_at {
+        let offset = self.write_at
+        for i in 0..offset {
             self.data.swap(i, offset+i);
         }
         self.write_at = 0;
