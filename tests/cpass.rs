6c2c0774753df16bcf43f212e07dab0a8e86a971
Sosthène Guédon <sosthene@nitrokey.com>
2024-07-01 16:31:48 +0200

    Queue: implement QueueView on top of #486

--- tests/cpass.rs
+++ tests/cpass.rs
@@ -1,8 +1,9 @@
 //! Collections of `Send`-able things are `Send`
 
 use heapless::{
-    spsc::{Consumer, Producer, Queue},
-    HistoryBuffer, Vec,
+    histbuf::HistoryBufferView,
+    spsc::{Consumer, ConsumerView, Producer, ProducerView, Queue, QueueView},
+    HistoryBuffer, Vec, VecView,
 };
 
 #[test]
@@ -13,13 +14,18 @@ fn send() {
 
     fn is_send<T>()
     where
-        T: Send,
+        T: Send + ?Sized,
     {
     }
 
     is_send::<Consumer<IsSend, 4>>();
+    is_send::<ConsumerView<IsSend>>();
     is_send::<Producer<IsSend, 4>>();
+    is_send::<ProducerView<IsSend>>();
     is_send::<Queue<IsSend, 4>>();
+    is_send::<QueueView<IsSend>>();
     is_send::<Vec<IsSend, 4>>();
+    is_send::<VecView<IsSend>>();
     is_send::<HistoryBuffer<IsSend, 4>>();
+    is_send::<HistoryBufferView<IsSend>>();
 }
