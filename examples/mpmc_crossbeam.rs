425c851565383d57c72422e80e44e399c648da53
0xllx0 <github+elle@weathered-steel.dev>
2025-10-29 02:51:29 +0000

    examples: add MPMC variant examples
    Adds examples for code size comparison of MPMC variants.

---
+++ examples/mpmc_crossbeam.rs
@@ -0,0 +1,12 @@
+use heapless::mpmc::Queue;
+
+fn main() {
+    let q = Queue::<_, 2>::new();
+    for _ in 0..255 {
+        assert!(q.enqueue(0).is_ok());
+        assert_eq!(q.dequeue(), Some(0));
+    }
+
+    // Queue is empty, this should not block forever.
+    assert_eq!(q.dequeue(), None);
+}
