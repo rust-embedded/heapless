fb280ff825b7cd4873a885e63a3c651daf5d9a2b
Bradley Gannon <bradley@bradleygannon.com>
2026-08-05 17:09:46 -0400

    Extend len_type to support removal of mpmc_large

--- src/len_type.rs
+++ src/len_type.rs
@@ -1,8 +1,13 @@
 use core::{
     fmt::{Debug, Display},
     mem,
-    ops::{Add, AddAssign, Sub, SubAssign},
+    ops::{Add, AddAssign, BitAnd, Sub, SubAssign},
 };
+
+#[cfg(not(feature = "portable-atomic"))]
+use core::sync::atomic::{AtomicU16, AtomicU32, AtomicU8, AtomicUsize, Ordering};
+#[cfg(feature = "portable-atomic")]
+use portable_atomic::{AtomicU16, AtomicU32, AtomicU8, AtomicUsize, Ordering};
 
 #[allow(non_camel_case_types)]
 pub enum TypeEnum {
@@ -24,9 +29,11 @@ pub trait Sealed:
     + PartialEq
     + Add<Output = Self>
     + AddAssign
+    + BitAnd<Self, Output = Self>
     + Sub<Output = Self>
     + SubAssign
     + PartialOrd
+    + Ord
     + TryFrom<usize, Error: Debug>
     + TryInto<usize, Error: Debug>
 {
@@ -38,6 +45,11 @@ pub trait Sealed:
     const MAX_USIZE: usize;
     /// This type as an enum.
     const TYPE: TypeEnum;
+
+    /// The corresponding atomic integer type.
+    type Atomic: Atomic<Self, Self::Atomic>;
+    /// The corresponding signed integer type.
+    type Signed;
 
     /// The one value of the integer type.
     ///
@@ -66,16 +78,100 @@ pub trait Sealed:
             Some(self.into_usize())
         }
     }
+
+    /// Wrapping (modular) addition. Computes `self + rhs`, wrapping around at `Self::MAX_USIZE`.
+    #[inline]
+    fn wrapping_add(self, rhs: Self) -> Self {
+        Self::from_usize(self.into_usize().wrapping_add(rhs.into_usize()) & Self::MAX_USIZE)
+    }
+
+    /// Compare `seq` and `expected_pos` as if they were signed integers, returning the result of
+    /// `(seq as Signed).wrapping_sub(expected_pos as Signed).cmp(&0)`.
+    #[inline]
+    fn signed_wrapping_cmp(seq: Self, expected_pos: Self) -> core::cmp::Ordering {
+        match seq.into_usize().wrapping_sub(expected_pos.into_usize()) {
+            0 => core::cmp::Ordering::Equal,
+            d if d > (Self::MAX_USIZE / 2) => core::cmp::Ordering::Less,
+            _ => core::cmp::Ordering::Greater,
+        }
+    }
 }
 
+// TODO consider replacing with stdlib version once generic_atomic lands in stable
+// (https://github.com/rust-lang/rust/issues/130539)
+pub trait Atomic<T, A> {
+    /// Loads a value from the atomic integer.
+    ///
+    /// Behavior must be identical to the corresponding `load` implementation for the underlying
+    /// atomic type.
+    fn load(&self, order: Ordering) -> T;
+    /// Stores a value into the atomic integer.
+    ///
+    /// Behavior must be identical to the corresponding `store` implementation for the underlying
+    /// atomic type.
+    fn store(&self, val: T, order: Ordering);
+    /// Stores a value into the atomic integer if the current value is the same as the current
+    /// value.
+    ///
+    /// Behavior must be identical to the corresponding `compare_exchange_weak` implementation for
+    /// the underlying atomic type.
+    #[cfg(any(target_has_atomic = "ptr", feature = "portable-atomic"))]
+    fn compare_exchange_weak(
+        &self,
+        current: T,
+        new: T,
+        success: Ordering,
+        failure: Ordering,
+    ) -> Result<T, T>;
+}
+
+macro_rules! impl_atomic {
+    ($($(#[$meta:meta])* ($T:ty, $A:tt)),*) => {$(
+        impl Atomic<$T, $A> for $A {
+            fn load(&self, order: Ordering) -> $T {
+                self.load(order)
+            }
+            fn store(&self, val: $T, order: Ordering) {
+                self.store(val, order)
+            }
+            fn compare_exchange_weak(&self, current: $T, new: $T, success: Ordering, failure: Ordering) -> Result<$T, $T> {
+                self.compare_exchange_weak(current, new, success, failure)
+            }
+        }
+    )*}
+}
+
+/// Converts a `usize` into the atomic type associated with the given `LenType`.
+pub const fn new_atomic_lentype<L: LenType>(val: usize) -> L::Atomic {
+    unsafe {
+        match L::TYPE {
+            TypeEnum::u8 => mem::transmute_copy(&AtomicU8::new(val as u8)),
+            TypeEnum::u16 => mem::transmute_copy(&AtomicU16::new(val as u16)),
+            TypeEnum::u32 => mem::transmute_copy(&AtomicU32::new(val as u32)),
+            TypeEnum::usize => mem::transmute_copy(&AtomicUsize::new(val)),
+        }
+    }
+}
+
+impl_atomic!(
+    (u8, AtomicU8),
+    (u16, AtomicU16),
+    #[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
+    (u32, AtomicU32),
+    (usize, AtomicUsize)
+);
+
 macro_rules! impl_lentype {
-    ($($(#[$meta:meta])* $LenT:ident),*) => {$(
+    ($($(#[$meta:meta])* ($ULenT:tt, $SLenT:ty, $AULenT:ty)),*) => {$(
         $(#[$meta])*
-        impl Sealed for $LenT {
+        impl Sealed for $ULenT {
             const ZERO: Self = 0;
             const MAX: Self = Self::MAX;
             const MAX_USIZE: usize = Self::MAX as _;
-            const TYPE: TypeEnum = TypeEnum::$LenT;
+            const TYPE: TypeEnum = TypeEnum::$ULenT;
+
+            type Atomic = $AULenT;
+            type Signed = $SLenT;
 
             fn one() -> Self {
                 1
@@ -83,7 +179,7 @@ macro_rules! impl_lentype {
         }
 
         $(#[$meta])*
-        impl LenType for $LenT {}
+        impl LenType for $ULenT {}
     )*}
 }
 
@@ -102,11 +198,11 @@ pub trait LenType: Sealed + Zeroize {}
 pub trait LenType: Sealed {}
 
 impl_lentype!(
-    u8,
-    u16,
+    (u8, i8, AtomicU8),
+    (u16, i16, AtomicU16),
     #[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
-    u32,
-    usize
+    (u32, i32, AtomicU32),
+    (usize, isize, AtomicUsize)
 );
 
 pub const fn check_capacity_fits<LenT: LenType, const N: usize>() {
